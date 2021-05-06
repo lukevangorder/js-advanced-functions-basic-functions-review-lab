c02549c49594f87f9628865be61073fcf826ae83
lukevangorder <lukevangorder@gmail.com>
2021-05-06 15:21:25 -0400

    commit tree b893dc5073778598b37b9c01f4fe36c390486d56

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work="go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(flair="*") {
+    return function(adj="special") {
+        let adje = flair + adj + flair;
+        return `You are ${adje}!`;
+    };
+}
+
+const Calculator = {
+    add: function(a,b) {
+        return a+b;
+    },
+    subtract: function(a,b) {
+        return a-b;
+    },
+    multiply: function(a,b) {
+        return a*b;
+    },
+    divide: function(a,b) {
+        return a/b;
+    }
+}
+
+function actionApplyer(int, arr) {
+    if (arr.length === 0) {
+        return int;
+    } else {
+        for(const fn of arr ) {
+            int = fn(int);
+        }
+        return int;
+    }
+}
